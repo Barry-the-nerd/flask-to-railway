5bb0ef5ae4cae7447af90d64e9a73249bd338693
BarryChum <barrychum@users.noreply.github.com>
2024-07-18 13:29:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -273,4 +273,5 @@ def handler_verify_authentication():
     return {"verified": True}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
